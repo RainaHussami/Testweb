4751d327b6e6ce5c2db8131d3c280e18bd872327
RainaHussami <127688268+RainaHussami@users.noreply.github.com>
2023-07-31 14:13:02 -0400

    testweb

---
+++ index.js
@@ -0,0 +1,106 @@
+alert ("Welcome!")
+$(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#scroll1").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".png").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+  
+  
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#scroll2").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".lastsect").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+  
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#scroll3").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".mywork").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#scroll4").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".footer").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $(".about-me-button").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".png").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#footer1").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".png").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#footer2").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".lastsect").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+  $(document).ready(function() {
+    // Smooth scroll to About Me section when clicking on "About Me" link
+    $("#footer3").on("click", function(event) {
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: $(".mywork").offset().top
+        },
+        1000 // Adjust the duration of the slide animation in milliseconds (2 seconds in this case)
+      );
+    });
+  });
+
+  
